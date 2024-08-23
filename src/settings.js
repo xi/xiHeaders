d6d43b4113fc2b20287b662cab38287bd5831d1f
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-08-23 16:25:59 +0200

    fix default rules value

--- src/settings.js
+++ src/settings.js
@@ -4,7 +4,7 @@ var form = document.querySelector('form');
 var textarea = document.querySelector('textarea');
 
 browser.storage.local.get('rules').then(data => {
-    var rules = data.rules || {};
+    var rules = data.rules || [];
     textarea.value = JSON.stringify(rules, null, 2);
 });
 
